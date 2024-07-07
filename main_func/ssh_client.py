a4baa3b961bcf7903a2f781a97aca80b48db9c33
kolo <sabatt148@gmail.com>
2024-07-08 01:25:29 +0300

    dich

--- main_func/ssh_client.py
+++ main_func/ssh_client.py
@@ -1,24 +1,26 @@
 import asyncio
+import re
+
 from aiogram import types
 import paramiko
 import html
 
 
 async def connect_ssh(host, port, user, pswd):
-
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
     ssh.connect(hostname=host,
                 username=user,
                 password=pswd,
-                port=port)
+                port=port,
+                look_for_keys=False,
+                allow_agent=False)
 
     return ssh
 
 
 async def read_initial_output(chan):
-
     chan.send('')
 
     await asyncio.sleep(0.1)
@@ -29,7 +31,6 @@ async def read_initial_output(chan):
 
 
 async def run_interactive_session(message: types.Message, ssh_entity):
-
     channel = ssh_entity.invoke_shell()
     channel.settimeout(0.0)
 
@@ -48,20 +49,35 @@ async def run_interactive_session(message: types.Message, ssh_entity):
 
         while True:
 
+            await asyncio.sleep(0.5)
+
             if channel.recv_ready():
 
                 output = channel.recv(1024).decode('utf-8')
 
-                print('\n'.join(output.split('\n')[1:]), end='')
+                output = html.escape('\n'.join(output.split('\n')[1:-1]))
 
-                await message.answer(f"<code>bash</code>"
-                                     f"<pre>{html.escape('\n'.join(output.split('\n')[1:-1]))}</pre>",
-                                     parse_mode='HTML', end='')
+                output = re.sub('(\\[\\d{1,2};\\d{1,2}m)|(\\[\\dm)|(\\[\\?2004l)', '', output)
+
+                output = output.strip()
+
+                print(output, end='')
+
+                if output:
+
+                    await message.answer(f"<code>bash</code>"
+                                         f"<pre>{output}</pre>",
+                                         parse_mode='HTML', end='')
+                else:
+
+                    await message.answer(f'<pre>{html.escape(">>>")}</pre>',
+                                         parse_mode='HTML')
 
                 if output.strip().endswith(('>>>', '$', '#', '>', ':')):
                     break
+            else:
 
-            await asyncio.sleep(0.1)
+                break
 
     channel.close()
 
@@ -71,12 +87,11 @@ async def ssh_client(message: types.Message,
                      port,
                      username,
                      password):
-
     object_ssh = await connect_ssh(
-                             host,
-                             port,
-                             username,
-                             password)
+        host,
+        port,
+        username,
+        password)
 
     await run_interactive_session(message, object_ssh)
 
